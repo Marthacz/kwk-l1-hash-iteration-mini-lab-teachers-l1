e659c9cee7c2c47ae6bba24001c7649d226f3bfc
Martha Czernuszenko <mvczer@gmail.com>
2018-08-05 03:34:52 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,33 +1,14 @@
-prices = [20.0, 15.99, 2.50]
-item_names = ["Outrageous Shoes", "Beguiling Socks", "Chrome Laces"]
- 
-items = {}
- 
-idx = 0
-item_names.each do |name|
-  items[name] = prices[idx]
-  idx += 1
-end
- 
-# => {"Outrageous Shoes"=>20.0, "Beguiling Socks"=>15.99, "Chrome Laces"=>2.5}
 
 def create_olympics_hash
-  place = [Syd]
- 
-  
-end
+  place = [:Sydney,:Athens,:Beijing,:London]
+  year = [:2000,:2004,:2008,:2012]
+  olympics = {}
+  idx = 0
+  olympics.each do |place|
+    olympics[place] = year[idx]
+    idx+=1 
+end 
 
-  # Implement this method so that it returns a hash with the data provided on README.md
-
-
-
-
-|Place          | Year      |
-| ------------- |:---------:|
-| Sydney        | 2000      |
-| Athens        | 2004      |
-| Beijing       | 2008      |
-| London        | 2012      |
 
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
